3a7857622fd29029a67aed55147429dd44436ffe
Sapthesh V <sapthesh@users.noreply.github.com>
2026-04-25 11:56:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-        base: /GhostData/,
+      base: '/GhostData/',
       plugins: [react()],
       resolve: {
         alias: {
